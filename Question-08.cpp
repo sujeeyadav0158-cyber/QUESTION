66f4e85ec897fa6bd7d0b33b1263f68c0b9001ed
Sujeet Yadav <sujeeyadav0158@gmail.com>
2026-06-10 17:48:36 +0530

    Question-08

---
+++ Question-08.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Payment
+{
+public:
+    virtual void pay()
+    {
+        cout<<"Payment Mode:"<<endl;
+    }
+};
+
+class CreditCardPayment : public Payment
+{
+public:
+    void pay()
+    {
+        cout << "Payment done using Credit Card" << endl;
+    }
+};
+
+class UPI : public Payment
+{
+public:
+    void pay()
+    {
+        cout << "Payment done using UPI" << endl;
+    }
+};
+
+class NetBanking : public Payment
+{
+public:
+    void pay()
+    {
+        cout << "Payment done using Net Banking" << endl;
+    }
+};
+
+int main()
+{
+    Payment *p;
+
+    CreditCardPayment c;
+    UPI u;
+    NetBanking n;
+
+    p = &c;
+    (*p).pay();
+
+    p = &u;
+    (*p).pay();
+
+    p = &n;
+    (*p).pay();
+
+    return 0;
+}

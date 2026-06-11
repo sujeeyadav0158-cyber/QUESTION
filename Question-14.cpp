e5958e2eac53ea860b9a2c7546a4273041f3f0c7
Sujeet Yadav <sujeeyadav0158@gmail.com>
2026-06-11 06:47:31 +0530

    Question-14

---
+++ Question-14.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class ProductB;   
+
+class ProductA
+{
+private:
+    int priceA;
+
+public:
+    void setPrice()
+    {
+        cout << "Enter Price of Product A: ";
+        cin >> priceA;
+    }
+
+    friend void compare(ProductA, ProductB);
+};
+
+class ProductB
+{
+private:
+    int priceB;
+
+public:
+    void setPrice()
+    {
+        cout << "Enter Price of Product B: ";
+        cin >> priceB;
+    }
+
+    friend void compare(ProductA, ProductB);
+};
+
+void compare(ProductA a, ProductB b)
+{
+    if (a.priceA > b.priceB)
+        cout << "Product A is Costlier"<<endl;
+    else if (b.priceB > a.priceA)
+        cout << "Product B is Costlier"<<endl;
+    else
+        cout << "Both Products have Same Price"<<endl;
+}
+
+int main()
+{
+    ProductA a;
+    ProductB b;
+
+    a.setPrice();
+    b.setPrice();
+
+    compare(a, b);
+
+    return 0;
+}
